16beebe2b89800f555799bb82985ccb807da9ebc
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-09 16:07:16 +0000

    fix#3: docstyle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""This module defines a class Square"""
+"""This module defines a class Square."""
 
 
 class Square():
-    """Defines a Square"""
+    """Defines a Square."""
+
     width = 0
     height = 0
 
     def __init__(self,**kwargs):
-        """Init method"""
+        """Init method."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square."""
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """ Permiter of the square """
+        """Permiter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string resprestation of the square."""
         return "{}/{}".format(self.width, self.height)
 
 
